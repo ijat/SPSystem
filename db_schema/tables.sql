9fc4300bf094da310879c1549c6f0ac782930ded
ijat <$y4f1q93>
2016-12-12 02:59:26 +0800

    Update db-table schema

--- db_schema/tables.sql
+++ db_schema/tables.sql
@@ -1,12 +1,14 @@
+drop database spsdb_v1;
+CREATE database spsdb_v1;
 USE spsdb_v1;
 CREATE TABLE user_data
 (
-UID int NOT NULL,
-USERNAME varchar(255) NOT NULL,
+UID int NOT NULL unique,
+USERNAME varchar(255) NOT NULL unique,
 FULLNAME text NOT NULL,
 PASS text NOT NULL,
-IR_ID int NOT NULL,
+IR_ID int NOT NULL unique,
 COUNTER int NOT NULL,
-PIC blob,
+UHASH TEXT NOT NULL,
 PRIMARY KEY (UID)
 )
